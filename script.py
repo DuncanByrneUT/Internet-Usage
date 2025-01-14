1722ec87fc3c16216800bc5e18e228d8768c2e38
DuncanByrneUT <duncanbyrne7254@gmail.com>
2025-01-14 15:01:00 -0500

    imported pandas,streamlit, and plotly. added code for nav sidebar, setup streamlit and data

--- script.py
+++ script.py
@@ -1,5 +1,19 @@
-# import pands
+# import pands, streamlit, nd plotly
 
 import pandas as pd
+import streamlit as st
+import plotly.express as px
 
 df = pd.read_csv('internet_users.csv')
+
+# preparing the data
+df['growth_rate'] = df['Internet Users'] / df.groupby("Country")('Internet Users').shift(1) -1
+
+# setting up streamlit
+st.title('Global Internet Usage Dashboard')
+
+# navisgation sidebar
+
+page = st.sidebar.selectbox("Choose a page", ["overview", "Disparities", "Growth Trends", "Importance of Internet Access" ])
+
+
