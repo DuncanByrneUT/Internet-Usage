3e1f98b5a08e60125fc8c6e1df4c9fd4c8ca6971
DuncanByrneUT <duncanbyrne7254@gmail.com>
2025-01-14 15:17:50 -0500

    added code for global stats and disparities

--- script.py
+++ script.py
@@ -16,4 +16,29 @@ st.title('Global Internet Usage Dashboard')
 
 page = st.sidebar.selectbox("Choose a page", ["overview", "Disparities", "Growth Trends", "Importance of Internet Access" ])
 
+# Global Stats
+if page == "Overview":
+    st.subheader("Global Internet Usage Statistics")
+    total_users = df['Internet Users'].sum()
+    st.write(f"Total Internet Users Worldwide: {total_users:,}")
+    avg_pen = df['Internet Penetration'].mean()
+    st.write(f"Average Internet Penetration: {avg_pen:.2f}%")
 
+    # global distribution plot
+    fig = px.choropleth(df, locations="Country", locationmode='country names',
+                        color='Internet Users', hover_name="Country",
+                        title="Internet Users by Country")
+    st.plotly_chart(fig)
+
+# disparities
+if page == "Disparities":
+    st.subheader("Disparities in Intenet Usage")
+    sorted_df = df.sort_values('Internet Penetration')
+    fig = px.bar(sorted_df, x='Country', y='Internet Penetration',
+                 title="Countries Ranked by Internet Penetration")
+    st.plotly_chart(fig)
+
+    st.write("Top 5 Countries with Lowest Internet Penetration:")
+    st.write(sorted_df.head())
+
+# growth trends
